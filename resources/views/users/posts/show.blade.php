1c59a98087b241c81a20f8e83cf193be952f36e7
kuragebread <hamurakku27@gmail.com>
2026-08-17 17:18:42 +0800

    壊れたデータの修復

--- resources/views/users/posts/show.blade.php
+++ resources/views/users/posts/show.blade.php
@@ -10,9 +10,6 @@
 
     <article class="post-detail">
         <div class="post-detail-media post-media">
-    {{-- Post --}}
-    <div class="row border shadow m-0 bg-white rounded-4 overflow-hidden" style="border-color: #ff007f !important;">
-        <div class="col p-0 border-end d-flex align-items-center justify-content-center" style="background-color: #fff;">
             @if($post->images->isNotEmpty())
                 <div id="carouselPost-{{ $post->id }}" class="carousel slide w-100" data-bs-ride="false">
                     <div class="carousel-inner">
@@ -55,43 +52,6 @@
                         <button type="submit" class="post-like-button" aria-label="Like this post">
                             @if ($post->isLiked())
                                 <i class="fa-solid fa-heart text-danger" data-liked="true"></i>
-
-        <div class="col-4 px-0 bg-white">
-            <div class="card border-0">
-                <div class="card-header bg-white py-3 border-bottom" style="border-color: #ff007f !important;">
-                    <div class="row align-items-center">
-                        <div class="col-auto">
-                            <a href="{{ route('profile.show', $post->user->id) }}">
-                                @if ($post->user->avatar)
-                                    <img src="{{ $post->user->avatar }}" alt="{{ $post->user->name}}" class="rounded-circle avatar-sm">
-                                @else
-                                    <i class="fa-solid fa-circle-user text-secondary icon-sm"></i>
-                                @endif
-                            </a>
-                        </div>
-                        <div class="col ps-0">
-                            <a href="{{ route('profile.show', $post->user->id) }}" class="text-decoration-none text-dark">
-                                {{ $post->user->name }}
-                            </a>
-                        </div>
-                        <div class="col-auto">
-                            @if (Auth::user()->id === $post->user->id)
-                                {{-- If youare the owner of the post, show edit and deltete --}}
-                                <div class="dropdown">
-                                    <button class="btn btn-sm shadow-none" data-bs-toggle="dropdown">
-                                        <i class="fa-solid fa-ellipsis"></i>
-                                    </button>
-                                    <div class="dropdown-menu">
-                                        <a href="{{ route('post.edit', $post->id) }}" class="dropdown-item">
-                                            <i class="fa-regular fa-pen-to-square"></i>Edit
-                                        </a>
-                                        <button class="dropdown-item text-danger" data-bs-toggle="modal" data-bs-target="#delete-post-{{ $post->id }}">
-                                            <i class="fa-regular fa-trash-can"></i>Delete
-                                        </button>
-                                    </div>
-                                    {{-- include modal here --}}
-                                    @include('users.posts.contents.modals.delete')
-                                </div>
                             @else
                                 <i class="fa-regular fa-heart" data-liked="false"></i>
                             @endif
